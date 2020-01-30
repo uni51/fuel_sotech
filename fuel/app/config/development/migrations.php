2df109320326c65f2deff46440d96f72563b07d7
uni51 <uni51@users.noreply.github.com>
2020-01-30 16:33:17 +0900

    マイグレーションの実行

---
+++ fuel/app/config/development/migrations.php
@@ -0,0 +1,26 @@
+<?php
+return array(
+  'version' => 
+  array(
+    'app' => 
+    array(
+      'default' => 
+      array(
+        0 => '001_create_articles',
+        1 => '002_create_users',
+        2 => '003_create_comments',
+        3 => '004_create_categories',
+        4 => '005_create_article_category',
+      ),
+    ),
+    'module' => 
+    array(
+    ),
+    'package' => 
+    array(
+    ),
+  ),
+  'folder' => 'migrations/',
+  'table' => 'migration',
+  'flush_cache' => false,
+);
